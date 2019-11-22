3c59a668d0de41c5ad4a3f14ef476b6222aa3308
Chet Corcos <ccorcos@gmail.com>
2019-11-21 18:02:58 -0800

    Basic mechanics of evaluating secret code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-const root = document.createElement("div")
-document.body.appendChild(root)
+// Expose these on the window so we don't have to re-bundle for the secret code.
+window.React = React
+window.ReactDOM = ReactDOM
+
+const secretCode = `
+function App2() {
+	return React.createElement("div", {}, "Secret!")
+}
+ReactDOM.render(React.createElement(App2, {}), document.body)
+`.trim()
 
 export function App() {
 	// const [{ x, y }, setMouse] = React.useState({ x: 0, y: 0 })
@@ -20,8 +28,9 @@ export function App() {
 		<div>
 			<h1>Hello World</h1>
 			<input placeholder="HERE" />
+			<button onClick={() => eval(secretCode)}>click</button>
 		</div>
 	)
 }
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, document.body)
